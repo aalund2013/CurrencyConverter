71a509101a022f12c1df5c5858b31cb45b392e4b
CAMILA ROMERO <camilaar@byui.edu>
2022-07-14 16:33:43 -0400

    form

---
+++ script.js
@@ -0,0 +1,26 @@
+
+// when user clicks, it calls function getresults
+convert.addEventListener("click", getResults);
+
+// function getresults
+function getResults() {
+	fetch(`${api}`)
+		.then(currency => {
+			return currency.json();
+		}).then(displayResults);
+}
+
+// display results after convertion
+function displayResults(currency) {
+	let fromRate = currency.rates[resultFrom];
+	let toRate = currency.rates[resultTo];
+	finalValue.innerHTML =
+	((toRate / fromRate) * searchValue).toFixed(2);
+	finalAmount.style.display = "block";
+}
+
+// when user click on reset button
+function clearVal() {
+	window.location.reload();
+	document.getElementsByClassName("finalValue").innerHTML = "";
+};
