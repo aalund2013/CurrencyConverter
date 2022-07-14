90d17c73680fcc6153819880f17c1af625e6a819
CAMILA ROMERO <camilaar@byui.edu>
2022-07-14 19:10:19 -0400

    form and results

--- src/js/content/currencyForm.js
+++ src/js/content/currencyForm.js
@@ -1,44 +1,30 @@
-const CurrencyForm = () => {
-    return (
-`
-<div class="currency-form-wrapper">
-    <form class="currency-form">
-        <h4>From: </h4>
-        <select name="from-currency" id="from-currency">
-            <option value="USD">USD</option>
-            <option value="EUR">EUR</option>
-            <option value="GBP">GBP</option>
-            <option value="CAD">CAD</option>
-            <option value="JPY">JPY</option>
-            <option value="AUD">AUD</option>
-            <option value="CNY">CNY</option>
-            <option value="INR">INR</option>
-            <option value="MXN">MXN</option>
-        </select>
 
-        <h4>Amount: </h4>
-        <input type="number" name="from-amount" id="from-amount" min="0" step="0.01" value="0">
+// include api for currency change
+const api = "https://api.exchangerate-api.com/v4/latest/USD";// when user clicks, it calls function getresults
+convert.addEventListener("click", getResults);
 
-        <h4>To: </h4>
-        <select name="to-currency" id="to-currency">
-            <option value="USD">USD</option>
-            <option value="EUR">EUR</option>
-            <option value="GBP">GBP</option>
-            <option value="CAD">CAD</option>
-            <option value="JPY">JPY</option>
-            <option value="AUD">AUD</option>
-            <option value="CNY">CNY</option>
-            <option value="INR">INR</option>
-            <option value="MXN">MXN</option>
-        </select>
+// function getresults
+function getResults() {
+	fetch(`${api}`)
+		.then(currency => {
+			return currency.json();
+		}).then(displayResults);
+}
 
-        <h4 class="result-header">Result: </h4>
-        <input type="number" name="to-amount" id="to-amount" min="0" step="0.01" value="0" readonly>
-    </form>
-    <button class="convert-button">Convert</button>
+// display results after convertion
+function displayResults(currency) {
+	let fromRate = currency.rates[resultFrom];
+	let toRate = currency.rates[resultTo];
+	finalValue.innerHTML =
+	((toRate / fromRate) * searchValue).toFixed(2);
+	finalAmount.style.display = "block";
+}
 
-</div>
-`
-);}
+// when user click on reset button
+function clearVal() {
+	window.location.reload();
+	document.getElementsByClassName("finalValue").innerHTML = "";
+};
+
 
 document.querySelector('.home-content').innerHTML = CurrencyForm();
